0da6bdcb26b7367f9133307880baa067a80d49dc
mcsky23 <vladseba23@gmail.com>
2025-11-02 01:06:16 +0200

    initial corpus minify and process

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ mod parsing;
 mod mutators;
 mod utils;
 
+use core::num;
 use std::fs;
 use tokio::fs::File as TokioFile;
 use tokio::io::AsyncWriteExt;
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::sync::mpsc;
 use std::path::PathBuf;
 use tokio::task::JoinHandle;
 use clap::Parser;
@@ -24,15 +25,23 @@ use crate::mutators::minifier::Minifier;
 struct Args {
     #[arg(short, long)]
     output_dir: PathBuf,
+    
     // overwrite existing corpus files and start from scratch
     #[arg(short, long, action=clap::ArgAction::SetTrue)]
     overwrite: Option<bool>,
+    // if overwrite is true, we need an initial corpus directory to read from
+    #[arg(short, long)]
+    initial_corpus: Option<PathBuf>,
+    
     // resume from existing corpus
     #[arg(short, long, action)]
     resume: Option<bool>,
     // the profile to use
     #[arg(short, long)]
-    profile: Option<String>,
+    profile: String,
+    // number of workers
+    #[arg(short, long, default_value_t = 1)]
+    workers: usize,
 }
 
 #[tokio::main]
@@ -40,9 +49,17 @@ async fn main() {
     let args = Args::parse();
     let output_dir = args.output_dir;
     let mutators = vec![
-        Arc::new(NumericTweaker),
+    Arc::new(NumericTweaker),
     ];
-
+    
+    let profile = profiles::get_profile(&args.profile).expect("Failed to get profile");
+    let pool_size = args.workers;
+    
+    let mut pool = runner::pool::FuzzPool::new(
+        pool_size,
+        &profile
+    ).expect("Failed to create FuzzPool");
+    
     if args.overwrite.unwrap_or(false) {
         // check if the directory exists
         if output_dir.exists() {
@@ -61,21 +78,87 @@ async fn main() {
         } else {
             fs::create_dir_all(&output_dir).unwrap();
         }
-
-        // read initial corpus, minify and then check if the engine 
-        // can execute it without syntax errors
+        
+        // read initial corpus, minify and then check if the engine can execute it without syntax errors
+        let start = std::time::Instant::now();
+        let mut num_files = 0;
+        let mut err_cnt = 0;
+        let intial_corpus_dir = args.initial_corpus.expect("Initial corpus directory must be provided when overwrite is set");
+        let mut job_handles = vec![];
+        
+        // start the ingestor task
+        let (job_result_tx, mut job_result_rx) = mpsc::channel::<(PathBuf, JobResult, Vec<u8>)>(5000);
+        tokio::task::spawn(init_job_result_ingestor(output_dir, job_result_rx));
+        
+        for entry in fs::read_dir(intial_corpus_dir).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            let path_clone = path.clone();
+            let src = fs::read(&path).unwrap();
+            let src_clone = src.clone();
+            let script = parse_js(String::from_utf8(src).unwrap());
+            if script.is_err() {
+                err_cnt += 1;
+                continue;
+            }
+            let script = script.unwrap();
+            
+            let minified_script = Minifier::mutate(script);
+            if let Err(e) = minified_script {
+                unreachable!("Minification failed: {:?}", e);
+            }
+            let minified_script = minified_script.unwrap();
+            
+            let new_code = generate_js(minified_script);
+            if let Err(e) = new_code {
+                unreachable!("Code generation failed: {:?}", e);
+            }
+            let new_code = new_code.unwrap();
+            
+            // schedule a job to check if the engine can execute it
+            let mut result_tx = pool.schedule_job(new_code.clone()).await.unwrap();
+            let job_result_tx_clone = job_result_tx.clone();
+            let handle = tokio::task::spawn(async move {
+                let job_result = result_tx.recv().await.unwrap();
+                if let Err(_) = job_result {
+                    panic!("Engine failed to execute minified script from {:?}, original size: {}, minified size: {}", path_clone, src_clone.len(), new_code.len());
+                }
+                let job_result = job_result.unwrap();
+                if job_result.status_code != 0 {
+                    return;
+                }
+                job_result_tx_clone.send((path_clone, job_result, new_code)).await.unwrap();
+            });
+            job_handles.push(handle);
+            
+            if job_handles.len() >= 10000 {
+                for handle in job_handles.drain(..) {
+                    if let Err(err) = handle.await {
+                        eprintln!("Result task panicked: {:?}", err);
+                    }
+                }
+                println!("Processed {} files...", num_files);
+            }
+            num_files += 1;
+        }
+        for handle in job_handles {
+            handle.await.unwrap();
+        }
+        let duration = start.elapsed();
+        println!("Processed {} files in {:?}", num_files, duration);
+        println!("{} files had errors and were skipped", err_cnt);
     }
+    
+}
 
-
-    // let src: Vec<u8> = fs::read("test.js").unwrap();
-    // let script = parse_js(String::from_utf8(src).unwrap());
-    // let new_script = NumericTweaker::mutate(script.unwrap());
-    // // println!("Mutated Script: {:#?}", new_script.unwrap());
-    // // println!("Mutated Script: {:#?}", new_script.unwrap());
-    // let new_code = generate_js(new_script.unwrap());
-    // fs::write("test_out.js", new_code.unwrap()).unwrap();
-    // // let duration = start_time.elapsed();
-    // // println!("Executed {} jobs in {:?}", cnt, duration);
-    // // println!("{} errors", err);
-
+/// Ingests initial job results and writes successful scripts to the output directory
+pub async fn init_job_result_ingestor(
+    output_dir: PathBuf, mut result_rx: mpsc::Receiver<(PathBuf, JobResult, Vec<u8>)>
+) {
+    while let Some((job_path, _job_result, script_bytes)) = result_rx.recv().await {
+        let mut output_path = output_dir.clone();
+        output_path.push(job_path.file_name().unwrap());
+        let mut output_file = TokioFile::create(output_path).await.unwrap();
+        output_file.write_all(&script_bytes).await.unwrap();
+    }
 }
--- src/profiles/mod.rs
+++ src/profiles/mod.rs
@@ -1,2 +1,9 @@
 pub mod v8;
 pub mod profile;
+
+pub fn get_profile(name: &str) -> Option<impl profile::JsEngineProfile> {
+    match name {
+        "v8" => Some(v8::V8Profile),
+        _ => None,
+    }
+}
